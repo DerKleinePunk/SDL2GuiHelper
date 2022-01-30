7629d5d6b0382e1ab7691b87469c65548b3087bc
DerKleinePunk <roboter@mine-robo.de>
2022-01-30 12:02:48 +0100

    update cmake and easylogging

--- cmake/cppcheckhelper.cmake
+++ cmake/cppcheckhelper.cmake
@@ -1,14 +1,16 @@
 find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
 
 add_custom_target(
-        cppcheck ALL
+        cppcheck warning,style,performance,information,unusedFunction
         COMMAND ${CMAKE_CXX_CPPCHECK}
-        --enable=warning,performance,portability,information,missingInclude
-        --std=c++11
-        --project=compile_commands.json
-        --template=gcc
-        --quiet
-        --library=${CMAKE_SOURCE_DIR}/tools/cppcheck/sdl.cfg
+        -DELPP_THREAD_SAFE
         -DELPP_CXX11
+        --project=compile_commands.json
+        -i${CMAKE_SOURCE_DIR}/src/common/easylogging
+        --std=c++11
+        --enable=warning,performance,portability,information
         --suppress-xml=${CMAKE_SOURCE_DIR}/tools/cppcheck/CppCheckSuppressions.xml
+        --verbose
+        --template=gcc
+        --library=${CMAKE_SOURCE_DIR}/tools/cppcheck/sdl.cfg
 )
